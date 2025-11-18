0970f54f24ed1181cf456bc9847044fa4f7f9136
Leo <info@furensic.eu>
2025-11-18 18:02:40 +0100

    Created /health endpoint + handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"log"
+	"net/http"
 )
 
 func main() {
-	log.Fatal("Not implemented")
+	log.Print("Starting url shortener service")
+	mux := http.NewServeMux()
+
+	log.Print("Mounting handlers")
+	mux.HandleFunc("GET /health", getHealth)
+
+	log.Print("Creating http server")
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Print("Starting http server on ", server.Addr)
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal("Error listening and serving:", err.Error())
+	}
+}
+
+func getHealth(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Application running!\n"))
 }
